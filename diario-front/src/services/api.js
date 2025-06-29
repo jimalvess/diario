2d76bc609a39b2291d4e8401d97bbd437f06ad7c
Jim Alves <jimalvess@gmail.com>
2025-06-29 18:45:55 -0300

    ajustandfo o front pra pegar o back deployado

--- diario-front/src/services/api.js
+++ diario-front/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // back rodando
+  // Usa a variável de ambiente VITE_APP_API_URL. Ele expõe variáveis de ambiente que começam com VITE_ como import.meta.env.VITE_NOME_DA_VARIAVEL
+  // No deploy, usa o back deployado, ou, usa o local.
+  baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:8080",
 });
 
 export default api;
